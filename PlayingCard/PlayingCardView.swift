1f03383ba6775c8163335462a69f1f6a1b2a5f80
Alvin <trolommonm@gmail.com>
2018-07-14 21:58:15 +0800

    Added PlayingCardView

---
+++ PlayingCard/PlayingCardView.swift
@@ -0,0 +1,19 @@
+//
+//  PlayingCardView.swift
+//  PlayingCard
+//
+//  Created by Alvin Tan De Jun on 14/7/18.
+//  Copyright © 2018 Alvin Tan. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        
+    }
+
+}
